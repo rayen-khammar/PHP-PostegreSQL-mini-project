abc15336147abc1b421854477e5ad01e27708346
Rayen Khammar <khammarrayen4@gmail.com>
2026-03-28 16:50:17 +0100

    finalising

--- home.php
+++ home.php
@@ -12,6 +12,7 @@ if (!isset($_SESSION['user']) || !isset($_SESSION['role'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<?php include 'navbar.php'; ?>
 
 <body>
     <div class="container" >
--- index.php
+++ index.php
@@ -1,5 +1,9 @@
-<?php session_start(); ?>
-<!DOCTYPE html>
+<?php session_start(); 
+if (!isset($_SESSION['user']) || !isset($_SESSION['role'])) {
+    header("Location: index.php");
+    exit();}
+?>
+<!DOCTYPE html>  
 <?php $mode = $_COOKIE['mode'] ?? 'dark'; ?>
 <html data-bs-theme="dark" lang="en">
 <head>
@@ -8,28 +12,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
+<?php include 'navbar.php'; ?>
 <body>
     <div class="container" >
-    <?php if (isset($_GET['error'])): ?>
-        <div class="alert alert-danger alert-dismissible fade show mt-3">
-            <strong>Error</strong>
-            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-            <br>
-            <small><?php echo htmlspecialchars($_GET['error']); ?></small>
-        </div>
-    <?php endif; ?>
-    <form action="processLogin.php" method="POST">
-        <div class="mb-3">
-            <label for="username" class="form-label">Username</label>
-            <input type="username" name="user" class="form-control" id="username">
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" name="password" class="form-control" id="exampleInputPassword1">
-        </div>
-        <button type="submit" class="btn btn-primary">Login</button>
-    </form>
-    </div>
+    <p> Hello, PHP Lovers! Welcome to your administration Platform </p>
 </body>
 
 </html>
