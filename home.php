5e70da756a0edda1d849641897e096fb54192b46
Rayen Khammar <khammarrayen4@gmail.com>
2026-03-28 16:57:30 +0100

    some fixes

--- home.php
+++ home.php
@@ -13,16 +13,9 @@ if (!isset($_SESSION['user']) || !isset($_SESSION['role'])) {
     <title>Document</title>
 </head>
 <?php include 'navbar.php'; ?>
-
 <body>
     <div class="container" >
     <p> Hello, PHP Lovers! Welcome to your administration Platform </p>
-    <?php if($_SESSION['role'] == 'admin') : ?>
-        <a href="admin.php" class="btn btn-primary">Students List</a>
-    <?php else : ?>
-        <a href="normal.php" class="btn btn-primary">Students List</a>
-    <?php endif; ?>
-    </div>
 </body>
 
 </html>
